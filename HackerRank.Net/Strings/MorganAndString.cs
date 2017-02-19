5fd06073569989f9bf89a2e946d7926529bc75c8
alphaCoder <srao2013@gmail.com>
2017-02-18 19:05:34 -0500

    Morgan and string

--- HackerRank.Net/Strings/MorganAndString.cs
+++ HackerRank.Net/Strings/MorganAndString.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,31 +9,71 @@ namespace HackerRank.Net.Strings
 {
     public class MorganAndString
     {
-       public static string ArrangeLexicographically(string s1, string s2)
+        public static string ArrangeLexicographically(string s1, string s2)
         {
-            s1 = s1 + "$";
-            s2 = s2 + "$";
-            StringBuilder sb = new StringBuilder();
+            s1 = s1 + "z";
+            s2 = s2 + "z";
+
+            StringBuilder sb = new StringBuilder(s1.Length + s2.Length);
             int i = 0, j = 0;
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
             while (i < s1.Length - 1 && j < s2.Length - 1)
             {
-                if (Compare(s1, s2, i, j) < 0)
+                if (s1[i] < s2[j])
                 {
-                    sb.Append(s1[i++].ToString());
+                    sb.Append(s1[i++]);
                 }
-                else {
-                    sb.Append(s2[j++].ToString());
+                else if (s1[i] > s2[j])
+                {
+                    sb.Append(s2[j++]);
+                }
+                else
+                {
+                    if (Compare1(s1, s2, i, j) < 0)
+                    {
+                        sb.Append(s1[i++]);
+                        while (s1[i - 1] == s1[i] && i < s1.Length-1)
+                        {
+                            sb.Append(s1[i++]);
+                        }
+                    }
+                    else
+                    {
+                        sb.Append(s2[j++]);
+                        while (s2[j - 1] == s2[j] && j < s2.Length-1)
+                        {
+                            sb.Append(s2[j++]);
+                        }
+                    }
                 }
             }
+            stopwatch.Stop();
             sb.Append(s1.Substring(i, s1.Length - i - 1));
             sb.Append(s2.Substring(j, s2.Length - j - 1));
 
+            Console.WriteLine("elapsed time strings in  ms: {0}", stopwatch.ElapsedMilliseconds);
             return sb.ToString();
         }
-       private static int Compare(String a, String b, int i, int j)
+
+        private static int Compare(string a, string b, int i, int j)
         {
             int len1 = a.Length - i, len2 = b.Length - j;
-            return a.Substring(i, len1).CompareTo(b.Substring(j, len2));
+            int result = String.Compare(a.Substring(i, len1), b.Substring(j, len2), true);
+            return result;
+        }
+
+        static int Compare1(String a, String b, int i, int j)
+        {
+            int len1 = a.Length - i, len2 = b.Length - j, k = 0;
+            int lim = Math.Min(len1, len2);
+            while (k < lim)
+            {
+                char c1 = a[i + k], c2 = b[j + k];
+                if (c1 != c2) return c1 - c2;
+                k++;
+            }
+            return len2 - len1;
         }
     }
 }
